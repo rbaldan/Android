ebfcc013d4b86c4b5eb2517782b917ec971e8722
Rodrigo Baldan <rodrigo.baldan16@hotmail.com>
2016-11-04 09:38:38 -0200

    Meu Primeiro APP

---
+++ app/src/main/java/br/usjt/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,32 @@
+package br.usjt.meuprimeiroapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //constante static para identificar a mensagem
+    public final static String EXTRA_MESSAGE ="br.usjt.meuprimeiroapp.MESSAGE";
+    //será chamado quando o usuário clicar em Enviar
+    public void sendMessage(View view){
+
+
+    //será chamado quando o usuário clicar em Enviarpublic void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
+
